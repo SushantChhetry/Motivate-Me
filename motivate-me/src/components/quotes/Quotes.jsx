d2d0cc821aa9a429d22c0fa6125c0f0cd50e8387
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2022-05-26 13:19:00 -0500

    add: CSS added to quotes, background, and author

--- motivate-me/src/components/quotes/Quotes.jsx
+++ motivate-me/src/components/quotes/Quotes.jsx
@@ -1,27 +1,36 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import "./Quotes.css";
 
 const Quotes = () => {
   const baseURL = "https://type.fit/api/quotes";
   const ranIndex = Math.floor(Math.random() * 99) + 1;
 
   const [quotes, setQuotes] = useState(null);
+  const [button, setButton] = useState(false);
 
   useEffect(() => {
     axios.get(baseURL).then((response) => {
       setQuotes(response.data);
     });
-  }, []);
+  }, [button]);
 
   if (!quotes) return null;
 
   console.log(quotes);
 
   return (
-    <div className="Quotes">
-      <h1 className="Text">{quotes[ranIndex]["text"]}</h1>
-      <p className="Author">{quotes[ranIndex]["author"]}</p>
-      <button>Click ME!</button>
+    <div className="quotes">
+      <h1 className="text">" {quotes[ranIndex]["text"]} "</h1>
+      <p className="author">-{quotes[ranIndex]["author"]}</p>
+      <button
+        className="button"
+        onClick={() => {
+          setButton(!button);
+        }}
+      >
+        MOTIVATE ME!
+      </button>
     </div>
   );
 };
